d0564bd39241989fb98eae6149b7cf66b25cf873
DanBerkness <berknessdanielr@gmail.com>
2021-07-09 15:08:27 -0500

    Final Refactorization.

--- src/com/higherlowergame/objects/RunGame.java
+++ src/com/higherlowergame/objects/RunGame.java
@@ -6,43 +6,41 @@ import java.util.Scanner;
 public class RunGame {
 
 	public static void main(String[] args) {
-		Random randomNumber = new Random();
-		int theRandom = randomNumber.nextInt(100) + 1;
+		Random randomizer = new Random();
+		int randomNumber = randomizer.nextInt(100) + 1;
 		boolean win = false;
 		Scanner scanner = new Scanner(System.in);
 		
 		for (int tries = 5; tries > 0; tries--) { 
 			
 			System.out.println("Pick a number between 1 and 100");
-			
 			int userGuess = scanner.nextInt();
 			
-			if (userGuess == theRandom) { 
+			while(userGuess > 100 || userGuess < 1) {
+				System.out.println("Please enter a valid number between 1 and 100");
+				userGuess = scanner.nextInt();
+			}
+
+			if (userGuess == randomNumber) { 
 				win = true;
 				break;
 			}
+
+			else if (userGuess < randomNumber) {
+				System.out.println("Please pick a higher number");				 
+			}
+			else if (userGuess > randomNumber) {
+				System.out.println("Pick a lower number");
+			}
 			
-			if (userGuess > 100 || userGuess < 1) {
-				// User guessed outside of range
-				System.out.println("Please enter a valid number between 1 and 100");
-				 tries = tries + 1;
-				 continue;	 
-			}
-			if (tries > 1) {
-				if (userGuess < theRandom) {
-					System.out.println("Please pick a higher number");				 
-				}
-				if (userGuess > theRandom) {
-					System.out.println("Pick a lower number");
-				}
-			}
 		}
 		scanner.close();
 		if (win) {
 			System.out.println("You win!");	
 		}
 		else {
-			System.out.println("Sorry you lose");
+			System.out.println("You lose!");
+			System.out.println("The number to guess was: " + randomNumber);
 		}
 	}
 }
